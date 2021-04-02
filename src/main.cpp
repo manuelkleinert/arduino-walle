536ab115ce193eed0ea39707e7795eb9cdbac934
manuelkleinert <manuel.kleinert@gmail.com>
2021-04-02 22:26:07 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <ArduinoJson.h>
 #include <Adafruit_PWMServoDriver.h>
 
 int backA = 12;
 int backB = 13;
+
+int servoDefault = 0;
+int servoMax = 540;
+int servoMin = 50;
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
@@ -16,7 +21,13 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 // our servo # counter
 uint8_t servonum = 0;
 
+String incomingString = "";
+
+DynamicJsonDocument doc(1024);
+
 void setup() {
+  Serial.begin(115200);
+
   pinMode (backA, OUTPUT);
   pinMode (backB, OUTPUT);
 
@@ -28,21 +39,59 @@ void setup() {
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
 
-  pwm.setPWM(12, 0, 300);
-  pwm.setPWM(13, 0, 300);
+  pwm.setPWM(4, 0, 300);
+  pwm.setPWM(5, 0, 300);
+
+  pwm.setPWM(6, 0, 300);
+  pwm.setPWM(7, 0, 300);
+
+  Serial.println("<Arduino is ready>");
+
+  delay(500);
 }
 
 void loop() {
+  Serial.print(".");
 
-  pwm.setPWM(12, 0, 300);
-  pwm.setPWM(13, 0, 300);
+   // send data only when you receive data:
+  if (Serial.available()) {
+    // read the incoming byte:
+    incomingString = "";
+    while(Serial.available())
+    {
+      incomingString+= String((char)Serial.read());
+    }
 
-  delay(5000);
+    // say what you got:
+    Serial.print("START:: ");
+    Serial.print(incomingString);
 
-  pwm.setPWM(12, 0, 400);
-  pwm.setPWM(13, 0, 200);
+    deserializeJson(doc, incomingString);
+    JsonObject obj = doc.as<JsonObject>();
+    int servoId = obj["Servo"];
+    int position = obj["Pos"];
 
-  delay(5000);
+    Serial.println(" ::END");
+    Serial.flush();
+
+    if (servoMax >= position and servoMin <= position) {
+      pwm.setPWM(servoId, 0, position);
+    }
+  } else {
+    Serial.println("Serial Available false");
+  }
+
+  delay(200);
+
+  // pwm.setPWM(12, 0, 300);
+  // pwm.setPWM(13, 0, 300);
+
+  // delay(5000);
+
+  // pwm.setPWM(12, 0, 400);
+  // pwm.setPWM(13, 0, 200);
+
+  // delay(5000);
 
 /*    // Drive each servo one at a time using setPWM()
   Serial.println(servonum);
