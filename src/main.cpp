20c67e790fec38b551e3e61055a9952d39b62bb9
Manuel Kleinert <info@manuelkleinert.ch>
2021-04-22 21:31:45 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 #include <Wire.h>
 #include <ArduinoJson.h>
 #include <Adafruit_PWMServoDriver.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_ST7789.h>
+
+
+// int  TFT_DC = 7;     // TFT DC  pin is connected to NodeMCU pin D1 (GPIO5)
+// int  TFT_RST = 6;     // TFT RST pin is connected to NodeMCU pin D2 (GPIO4)
+// int  TFT_CS = 100;   // TFT CS  pin is connected to NodeMCU pin D8 (GPIO15)
+
+// Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
 
 int servoSwitchPin = 2;
 int motorPinA = 12;
@@ -10,6 +20,7 @@ int motorPinB = 13;
 int servoDefault = 300;
 int servoMax = 500;
 int servoMin = 100;
+
 
 /*
   Servos:
@@ -135,6 +146,13 @@ void setMotor() {
   }
 }
 
+// void testdrawtext(char *text, uint16_t color) {
+//   tft.setCursor(0, 0);
+//   tft.setTextColor(color);
+//   tft.setTextWrap(true);
+//   tft.print(text);
+// }
+
 void setup() {
   
   Serial.begin(115200);
@@ -157,6 +175,17 @@ void setup() {
   delay(500);
   setServos();
   delay(500);
+
+
+  // if the display has CS pin try with SPI_MODE0
+  // tft.init(240, 240, SPI_MODE2);    // Init ST7789 display 240x240 pixel
+ 
+  // // if the screen is flipped, remove this command
+  // tft.setRotation(2);
+
+  // tft.fillScreen(ST77XX_BLACK);
+  // testdrawtext("Wall-E", ST77XX_WHITE);
+
 
   Serial.println("<Wall-E is ready>");
   Serial.flush();
